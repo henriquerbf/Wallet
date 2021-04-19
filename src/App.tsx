5ded07e7e8529723e544dc0725b2e554034b85e3
henriquerbf <henriquerbf@hotmail.com>
2021-04-19 00:05:13 -0300

    Page default removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import GlobalStyles from './Styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 
-import Layout from './Components/Layout';
+import Routes from './Routes';
 import dark from './Styles/Themes/dark';
+// import light from './Styles/Themes/light';
 
-const App: React.FC = () =>{
+const App: React.FC = () => {
     return(
         <ThemeProvider theme={dark}>
             <GlobalStyles/>
-            <Layout/>
+            <Routes/>
         </ThemeProvider>//fragment <></> is used just to transport more than 1 tag
     );
 }
