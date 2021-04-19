55373d8144c48dbe3d5f131fde177c978394ec71
henriquerbf <henriquerbf@hotmail.com>
2021-04-19 00:09:04 -0300

    Aside Menu linking routes

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
+
+import {
+    Container,
+    Header,
+    LogImg,
+    MenuContainer,
+    MenuItemLink,
+    Title
+} from './styles';
+
+import logoImg from '../../Assets/logo.svg';
 
 const Aside: React.FC = () =>{
     return(
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt = "Logo Minha Carteira"/>
+                <Title>Wallet</Title>
+            </Header>
+            <MenuContainer>
+                <MenuItemLink href="/dashboard">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="/list/entry-balance">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="/list/exit-balance">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
 }
